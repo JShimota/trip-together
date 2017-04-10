d71f47e41dcff308539a4be3b815dacb19bb8243
nthh <nhearnsberger@gmail.com>
2017-04-10 01:27:32 -0400

    Update app.py

--- app.py
+++ app.py
@@ -19,5 +19,8 @@ def my_form_post():
 @app.route('/hello/<name>')
 def hello(name=None):
     return render_template('hello.html', name=name)
+
 if __name__ == '__main__':
-    app.run()
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
