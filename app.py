ee9512708174a8184cacf521d60e20e22655b895
nthh <nhearnsberger@gmail.com>
2017-04-10 00:53:19 -0400

    flask changes

--- app.py
+++ app.py
@@ -1,19 +1,8 @@
-from flask import Flask
-from flask import request
 from flask import render_template
 
-app = Flask(__name__)
-
-@app.route('/')
-def my_form():
-    return render_template("my-form.html")
-
-@app.route('/', methods=['POST'])
-def my_form_post():
-
-    text = request.form['text']
-    processed_text = text.upper()
-    return processed_text
-
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
 if __name__ == '__main__':
     app.run()
