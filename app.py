c948ead14b206d5c3fcd56498a84830fa12dc1dd
nthh <nhearnsberger@gmail.com>
2017-04-10 00:33:34 -0400

    Flask form addition

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
-import os
 from flask import Flask
+from flask import request
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello World'
+def my_form():
+    return render_template("my-form.html")
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+
+    text = request.form['text']
+    processed_text = text.upper()
+    return processed_text
 
 if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
