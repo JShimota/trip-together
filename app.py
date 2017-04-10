4da81f394be38f647523fb094c519a5ed2810786
nthh <nhearnsberger@gmail.com>
2017-04-10 01:22:08 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,19 @@
 from flask import render_template
 
+app = Flask(__name__)
+
+@app.route('/')
+def my_form():
+    return render_template("my-form.html")
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+
+    text = request.form['text']
+    processed_text = text.upper()
+    return processed_text
+
+
 @app.route('/hello/')
 @app.route('/hello/<name>')
 def hello(name=None):
